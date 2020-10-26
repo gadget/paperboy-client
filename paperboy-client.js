e904c79c96215226ad86e1fad5bfd1562a9d32cc
gadget <g4dg3t@gmail.com>
2020-10-26 19:14:02 +0100

    simpe and stupid reconnection logic

--- paperboy-client.js
+++ paperboy-client.js
@@ -3,6 +3,22 @@ class PaperboyClient {
   constructor(tokenBaseUrl, wsUrl) {
     this.tokenBaseUrl = tokenBaseUrl;
     this.wsUrl = wsUrl;
+    this.subscribed = false;
+  }
+
+  _scheduleSocketMaintainance() {
+    clearInterval(this._scheduleSocketMaintainanceInterval);
+    const that = this;
+    this._scheduleSocketMaintainanceInterval = setInterval(() => {
+      if (that.ws.readyState === 2 || that.ws.readyState === 3) {
+        console.log('Cleaning up WebSocket connection.');
+        that.ws.close();
+        if (that.subscribed === true) {
+          console.log('Reconnecting as client was subscribed.');
+          that.subscribe(that.channel, that.msgHandler);
+        }
+      }
+    }, 5000);
   }
 
   _requestToken(channel, callback) {
@@ -19,16 +35,30 @@ class PaperboyClient {
   _subscribe(token, msgHandler) {
     // TODO: use WSS for secure/encrypted ws channels
     const ws = new WebSocket(this.wsUrl);
+    this.ws = ws;
+    const that = this;
     ws.onopen = function() {
+      console.log('WebSocket opened.');
       ws.send(token);
+      that._scheduleSocketMaintainance();
     };
-    ws.onmessage = msgHandler;
-    // TODO: deal with reconnection, timeout, heartbeat, isalive, etc
-    // https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
+    ws.onclose = function() {
+      console.log('WebSocket closed.');
+    };
+    ws.onmessage = function(e) {
+      if (e.data.length === 10 && e.data === 'subscribed') {
+        console.log('Subscribed to channel.');
+        that.subscribed = true;
+      } else {
+        msgHandler(e);
+      }
+    };
   }
 
   subscribe(channel, msgHandler) {
-    const that = this
+    const that = this;
+    this.channel = channel;
+    this.msgHandler = msgHandler;
     this._requestToken(channel, function callback(token) {
       that._subscribe(token, msgHandler);
     })
