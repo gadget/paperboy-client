f0e1809a1357c9984d322ce8756a6804dcd47a18
gadget <g4dg3t@gmail.com>
2020-10-26 15:03:39 +0100

    minor changes

--- paperboy-client.js
+++ paperboy-client.js
@@ -1,21 +1,37 @@
-function requestToken(channel, callback) {
-  const http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(http.responseText);
-    }
-  }
-  http.open('GET', 'http://localhost:8080/paperboyAuth/testChannel');
-  http.send();
-}
+class PaperboyClient {
 
-function connect(token, msgHandler) {
-  // TODO: use WSS for secure/encrypted ws channels
-  ws = new WebSocket('ws://localhost:3000');
-  ws.onopen = function() {
-    ws.send(token);
-  };
-  ws.onmessage = msgHandler;
-  // TODO: deal with reconnection, timeout, heartbeat, isalive, etc
-  // https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
+  constructor(tokenBaseUrl, wsUrl) {
+    this.tokenBaseUrl = tokenBaseUrl;
+    this.wsUrl = wsUrl;
+  }
+
+  _requestToken(channel, callback) {
+    const http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        callback(http.responseText);
+      }
+    }
+    http.open('GET', this.tokenBaseUrl + channel);
+    http.send();
+  }
+
+  _subscribe(token, msgHandler) {
+    // TODO: use WSS for secure/encrypted ws channels
+    const ws = new WebSocket(this.wsUrl);
+    ws.onopen = function() {
+      ws.send(token);
+    };
+    ws.onmessage = msgHandler;
+    // TODO: deal with reconnection, timeout, heartbeat, isalive, etc
+    // https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
+  }
+
+  subscribe(channel, msgHandler) {
+    const that = this
+    this._requestToken(channel, function callback(token) {
+      that._subscribe(token, msgHandler);
+    })
+  }
+
 }
