9de47950935f42e014779a427e444b1d21d9959f
gadget <g4dg3t@gmail.com>
2020-11-01 00:57:18 +0100

    fixing subscription timing issue

--- paperboy-client.js
+++ paperboy-client.js
@@ -10,15 +10,15 @@ class PaperboyClient {
     clearInterval(this._scheduleSocketMaintainanceInterval);
     const that = this;
     this._scheduleSocketMaintainanceInterval = setInterval(() => {
-      if (that.ws.readyState === 2 || that.ws.readyState === 3) {
-        console.log('Dead WebSocket connection.');
+      if (that.ws != undefined && (that.ws.readyState === 2 || that.ws.readyState === 3)) {
+        console.log('Cleaning up dead WebSocket connection.');
         that.ws.close();
+        that.ws = undefined;
         if (that.subscriptions.size > 0) {
           console.log('Reconnecting as client was subscribed.');
-          for (let s of that.subscriptions.entries()) {
-            that.ws = undefined;
-            that.subscribe(s.key, s.value);
-          }
+          that.subscriptions.forEach((value, key) => {
+            that.subscribe(key, value);
+          });
         }
       }
     }, 5000);
@@ -35,40 +35,49 @@ class PaperboyClient {
     http.send();
   }
 
-  _subscribe(token, msgHandler) {
-    // TODO: use WSS for secure/encrypted ws channels
-    if (this.ws === undefined) {
-      const ws = new WebSocket(this.wsUrl);
-      this.ws = ws;
-      const that = this;
+  _openWebSocket(callback) {
+    const ws = new WebSocket(this.wsUrl);
+    this.ws = ws;
+    const that = this;
 
-      ws.onopen = function() {
-        console.log('WebSocket opened.');
-        that.ws.send(token);
-        that._scheduleSocketMaintainance();
-      };
-      ws.onclose = function() {
-        console.log('WebSocket closed.');
-      };
-      ws.onmessage = function(e) {
-        if (e.data.length < 100 && e.data.startsWith('subscribed:')) {
-          const ch = e.data.substring(11);
-          console.log('Subscribed to channel: "%s".', ch);
-        } else {
-          const msg = JSON.parse(e.data);
-          that.subscriptions.get(msg.channel)(msg);
-        }
-      };
-    } else {
-      this.ws.send(token);
-    }
+    ws.onopen = function() {
+      console.log('WebSocket opened.');
+      that._scheduleSocketMaintainance();
+      callback();
+    };
+    ws.onclose = function() {
+      console.log('WebSocket closed.');
+    };
+    ws.onmessage = function(e) {
+      if (e.data.length < 100 && e.data.startsWith('subscribed:')) {
+        const ch = e.data.substring(11);
+        console.log('Subscribed to channel: "%s".', ch);
+      } else {
+        const msg = JSON.parse(e.data);
+        that.subscriptions.get(msg.channel)(msg);
+      }
+    };
   }
 
   subscribe(channel, msgHandler) {
+    const tokenBuffer = [];
     this.subscriptions.set(channel, msgHandler);
     const that = this;
     this._requestToken(channel, function callback(token) {
-      that._subscribe(token, msgHandler);
+      if (that.ws === undefined) {
+        that._openWebSocket(function callback() {
+          while (tokenBuffer.length > 0) {
+            that.ws.send(tokenBuffer.pop());
+          }
+          that.ws.send(token);
+        });
+      } else {
+        if (that.ws.readyState === 1) {
+          that.ws.send(token);
+        } else {
+          tokenBuffer.push(token);
+        }
+      }
     })
   }
 
